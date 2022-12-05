e36a057120a7a00cb8aa2d1ef13c8969d91a76b3
ishaan <ishaansparihar22@gmail.com>
2022-12-05 22:53:46 +0530

    printing stars

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,9 @@
 public class JavaBasics {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
