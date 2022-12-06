e7de599f40b9996f962becc55ee110c1ad1349ab
ishaan <ishaansparihar22@gmail.com>
2022-12-06 23:37:56 +0530

    sum of two numbers added

---
+++ SumOftwoNumbers.java
@@ -0,0 +1,8 @@
+public class SumOftwoNumbers{
+    public static void main(String[] args) {
+        int firstnumber = 2;
+        int secondnumber = 3;
+        int sum = firstnumber+secondnumber;
+        System.out.println(sum);
+    }
+}
