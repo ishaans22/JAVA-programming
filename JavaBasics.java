658c49052e2ee7a3fd9fba52cc7365e70b1dd471
ishaan <ishaansparihar22@gmail.com>
2022-12-06 23:18:50 +0530

    variable in java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,9 +1,8 @@
 public class JavaBasics {
     public static void main(String[] args) {
-        System.out.println("*****");
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        int a = 5;
+        int b = 10;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
