d078f3581a8582f8c66f40e4526bb1cc7020247a
ishaan <ishaansparihar22@gmail.com>
2022-12-06 23:31:15 +0530

    data types in java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,8 +1,15 @@
 public class JavaBasics {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        System.out.println(a);
+        byte b = 8;
         System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price = (float) 1.5 ;
+        System.out.println(price);
+        int number =  25;
+        System.out.println(number);
+
     }
 }
