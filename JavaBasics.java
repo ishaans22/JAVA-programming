0f15689b5f4b34abaf2abd10eddb804cd586ca56
ishaan <ishaansparihar22@gmail.com>
2022-12-07 22:42:20 +0530

    input in java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,15 +1,11 @@
+import java.util.*;
+
 public class JavaBasics {
     public static void main(String[] args) {
-        byte b = 8;
-        System.out.println(b);
-        char ch = 'a';
-        System.out.println(ch);
-        boolean var = true;
-        System.out.println(var);
-        float price = (float) 1.5 ;
-        System.out.println(price);
-        int number =  25;
-        System.out.println(number);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+
 
     }
 }
